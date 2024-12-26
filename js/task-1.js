79b5518c0494d1469b4c19112c22ac6573166497
Yura Shaplavsky <yurikorock777@gmail.com>
2024-12-26 17:12:14 +0200

    add task 1, task 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,11 @@
-// function makeTransaction(quantity, pricePerDroid) {
-//     const totalPrice = quantity * pricePerDroid;
-//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-// };
-
-// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-
-  return totalPrice > customerCredits
-    ? "Insufficient funds!"
-    : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function slugify(title) {
+  const titleToLower = title.toLowerCase();
+  const wordsArray = titleToLower.split(" ");
+  const slug = wordsArray.join("-");
+  return slug;
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-2.js
+++ js/task-2.js
@@ -1,23 +1,9 @@
-// function getShippingMessage(country, price, deliveryFee) {
-//     const totalPrice = price + deliveryFee;
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = firstArray.concat(secondArray);
 
-//     return `Shipping to ${country} will cost ${totalPrice} credits`;
-// }
-
-// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength);
+  } else {
+    return newArray;
   }
-  return message.slice(0, maxLength) + "...";
 }
-
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
